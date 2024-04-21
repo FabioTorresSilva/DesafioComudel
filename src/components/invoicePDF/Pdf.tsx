a367a788b46eeaceb3dcfd9c1ad2603a97f876ef
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-04-21 19:45:59 +0100

    ts

--- src/components/invoicePDF/Pdf.tsx
+++ src/components/invoicePDF/Pdf.tsx
@@ -1,31 +1,29 @@
 import React from 'react';
-import { Page, Document, Image, StyleSheet } from '@react-pdf/renderer';
-import InvoiceTitle from './InvoiceTitle'
-import InvoiceNo from './InvoiceNo'
-import InvoiceItemsTable from './InvoiceItemsTable'
+import { Page, Document, Image, StyleSheet, ImageProps } from '@react-pdf/renderer';
+import InvoiceTitle from './InvoiceTitle';
+import InvoiceNo from './InvoiceNo';
+import InvoiceItemsTable from './InvoiceItemsTable';
 
-import logo from "../../public/olisipo.png"
+import logo from "../../public/olisipo.png";
 import InvoiceDescription from './InvoiceDescription';
-
 
 const styles = StyleSheet.create({
   page: {
-      fontFamily: 'Helvetica',
-      fontSize: 11,
-      paddingTop: 30,
-      paddingLeft:60,
-      paddingRight:60,
-      lineHeight: 1.5,
-      flexDirection: 'column',
-  }, 
+    fontFamily: 'Helvetica',
+    fontSize: 11,
+    paddingTop: 30,
+    paddingLeft: 60,
+    paddingRight: 60,
+    lineHeight: 1.5,
+    flexDirection: 'column',
+  },
   logo: {
-      width: 160,
-      height: 66,
-      marginLeft: 'auto',
-      marginRight: 'auto'
+    width: 160,
+    height: 66,
+    marginLeft: 'auto',
+    marginRight: 'auto',
   }
 });
-
 
 interface PdfProps {
   formData: {
@@ -44,20 +42,19 @@ interface PdfProps {
 }
 
 
+
 const Pdf: React.FC<PdfProps> = ({ formData, totalValue, invoiceNumber, invoiceDate }) => {
   const logoUrl = 'https://static-media.fluxio.cloud/olisipoway/_icon/share-b2a65c5bedbb26af91c68cece307864c.png';
   return (
     <Document>
-    <Page size="A4" style={styles.page}>
-    // @ts-ignore
-        <Image alt="logo" style={styles.logo} src={logoUrl} />
-        // @ts-ignore
-        <InvoiceTitle title="Fatura"/>
+      <Page size="A4" style={styles.page}>
+        <Image style={styles.logo} src={logoUrl} />
+        <InvoiceTitle />
         <InvoiceNo invoiceNumber={invoiceNumber} invoiceDate={invoiceDate} formData={formData} />
         <InvoiceItemsTable formData={formData} totalValue={totalValue} />
         <InvoiceDescription formData={formData} />
-    </Page>
-</Document>
+      </Page>
+    </Document>
   );
 };
 
